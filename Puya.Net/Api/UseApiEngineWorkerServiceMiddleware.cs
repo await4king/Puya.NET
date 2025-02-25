101471553b5f57cb521abda5b45e457caa4ab3dd
Masoud Bakhtiari <masoudbkhti@gmail.com>
2025-02-25 16:42:02 +0330

    adde PuyaApiEngine Middleware

---
+++ Puya.Net/Api/UseApiEngineWorkerServiceMiddleware.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Puya.Net.Api
+{
+    public static class ApiEngineWorkerServiceMiddlewareExtensions
+    {
+        public static IApplicationBuilder UsePuyaApiEngine(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ApiEngineWorkerServiceMiddleware>();
+        }
+    }
+}
