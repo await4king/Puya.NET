e9c0d232f2ae0314f04731562480981ca7acd361
Masoud Bakhtiari <masoudbkhti@gmail.com>
2025-02-26 12:36:43 +0330

    added IServiceScopeFactory to ApiEngineWorkerServiceMiddleware

--- Puya.Net/Api/ApiEngineWorkerServiceMiddleware.cs
+++ Puya.Net/Api/ApiEngineWorkerServiceMiddleware.cs
@@ -2,26 +2,38 @@
 using System.Threading.Tasks;
 using System.Threading;
 using Puya.Api;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Puya.Net.Api
 {
     public class ApiEngineWorkerServiceMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly IApiEngine _apiEngine;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
 
-        public ApiEngineWorkerServiceMiddleware(RequestDelegate next, IApiEngine apiEngine)
+        public ApiEngineWorkerServiceMiddleware(RequestDelegate next, IServiceScopeFactory serviceScopeFactory)
         {
             _next = next;
-            _apiEngine = apiEngine;
+            _serviceScopeFactory = serviceScopeFactory;
         }
 
         public async Task InvokeAsync(HttpContext context, CancellationToken cancellationToken)
         {
-            var response = await _apiEngine.Serve(context, cancellationToken);
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var apiEngine = scope.ServiceProvider.GetRequiredService<IApiEngine>();
 
-            context.Response.ContentType = "application/json";
-            await context.Response.WriteAsync(response);
+                if (apiEngine == null)
+                {
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsync("Internal Server Error: IApiEngine is missing.");
+                    return;
+                }
+                var response = await apiEngine.Serve(context, cancellationToken);
+
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(response);
+            }
         }
     }
 }
