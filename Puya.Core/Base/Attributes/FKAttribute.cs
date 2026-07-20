b019fdf636bd45a10e88afd054a984933958bcfd
puyacodes <developer@puyasoft.com>
2026-07-20 13:54:31 +0330

    released Puya.Core 3.9.4 released Puya.Net 4.9.4

---
+++ Puya.Core/Base/Attributes/FKAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Puya.Base
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FKAttribute : Attribute
+    {
+        public string ReferredTable { get; set; }
+        public string ReferredPk { get; }
+        public string ConstraintName { get; }
+
+        public FKAttribute(string referredTable, string referredPk = "", string constraint_name = "")
+        {
+            ReferredTable = referredTable;
+            ReferredPk = referredPk;
+            ConstraintName = constraint_name;
+        }
+    }
+}
---
+++ Puya.Core/Base/Attributes/PKAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Puya.Base
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class PKAttribute : Attribute
+    {
+        public PKAttribute()
+        { }
+        public PKAttribute(int order)
+        {
+            Order = order;
+        }
+
+        public int Order { get; }
+    }
+}
